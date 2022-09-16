67c741621b16b1ba419d842c938a236195d9da6c
chavesyuri-mb <79917740+chavesyuri-mb@users.noreply.github.com>
2022-09-16 16:41:49 -0400

    Initial Commit

---
+++ FaireWeather/FaireWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaireWeatherApp.swift
+//  FaireWeather
+//
+//  Created by Yuri Chaves on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FaireWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

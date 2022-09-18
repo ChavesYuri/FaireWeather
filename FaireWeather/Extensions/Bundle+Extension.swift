d1fe663d2ff3611f53f481a99b8b4937ba00d6d4
chavesyuri-mb <79917740+chavesyuri-mb@users.noreply.github.com>
2022-09-18 13:33:51 -0400

    Bundle Extension

---
+++ FaireWeather/Extensions/Bundle+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Bundle+Extension.swift
+//  FaireWeather
+//
+//  Created by Yuri Chaves on 18/09/22.
+//
+
+import Foundation
+extension Bundle {
+    var apiBaseUrl: String {
+        guard
+            let stringUrl = object(forInfoDictionaryKey: "API_URL") as? String
+        else {
+            fatalError("INVALID BASE API URL")
+        }
+        return stringUrl
+    }
+}
